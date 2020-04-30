0d3ac39dc876a1c8a4b2057f523692d793bfe080
UMASHIBA <umashiba1@gmail.com>
2020-04-30 18:56:14 +0900

    [SET]開発環境をセットアップ

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const outputPath = path.resolve(__dirname, "public");
+const HtmlWebpackPlugin = require("html-webpack-plugin"); //ここを追加
+
+module.exports = {
+  mode: "development",
+  entry: "./index.ts",
+  output: {
+    filename: "bundle.js",
+    path: `${outputPath}`,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  devServer: {
+    contentBase: `${outputPath}/`, //エントリーポイントを指定
+    open: true, // ブラウザを自動で開くか否か
+    hot: true, // 開発中にCSSとかを変更した時、リロードせずに更新するか否か
+    watchContentBase: true, // 変更した時自動でリロードされるか否か
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+  ],
+};
