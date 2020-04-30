4881048c39fecca5af90d7763ca99f0d03377335
UMASHIBA <umashiba1@gmail.com>
2020-05-01 03:45:09 +0900

    [ADD]h関数に関するテストを3つ追加

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,18 +1,67 @@
 import { h } from "../virtualDom";
 
-test(`h('div', {id: 'sample'}, []) return 
-{name: 'div', 
-props: {id: 'sample'}, 
-children: [], 
-realNode: null, 
-nodeType: null, 
-key: null}`, () => {
-  expect(h("div", { id: "sample" }, [])).toEqual({
+describe("h func test", () => {
+  // normalArguments
+  const name = "div";
+  const props = { id: "sample" };
+  const children: any[] = [];
+
+  const normalVNode = {
     name: "div",
     props: { id: "sample" },
     children: [],
     realNode: null,
     nodeType: null,
     key: null,
+  };
+
+  const sampleText = "sampleText";
+  const textVNode = {
+    name: sampleText,
+    props: {},
+    children: [],
+    realNode: null,
+    nodeType: 3,
+    key: null,
+  };
+  //
+
+  test("normalArguments return correct result", () => {
+    expect(h(name, props, children)).toEqual(normalVNode);
+  });
+
+  test("have VirtualNodeType children arguments return correct result", () => {
+    expect(
+      h(name, props, [h(name, props, children), h(name, props, children)])
+    ).toEqual({
+      name: "div",
+      props: { id: "sample" },
+      children: [normalVNode, normalVNode],
+      realNode: null,
+      nodeType: null,
+      key: null,
+    });
+  });
+
+  test("have string children arguments return correct result", () => {
+    expect(h(name, props, [sampleText, sampleText])).toEqual({
+      name: "div",
+      props: { id: "sample" },
+      children: [textVNode, textVNode],
+      realNode: null,
+      nodeType: null,
+      key: null,
+    });
+  });
+
+  test("have mixed children arguments return correct result", () => {
+    expect(h(name, props, [h(name, props, children), sampleText])).toEqual({
+      name: "div",
+      props: { id: "sample" },
+      children: [normalVNode, textVNode],
+      realNode: null,
+      nodeType: null,
+      key: null,
+    });
   });
 });
